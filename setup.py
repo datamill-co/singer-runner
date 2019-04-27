8f487742e321db254b210d8acd3ea8b85dbb8e87
Andrew Madonna <andrew.w.madonna@gmail.com>
2019-04-27 17:45:23 -0400

    Add setup.py args for pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 
+from os import path
 from distutils.core import setup
 from setuptools import find_packages
 
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='singer-runner',
+    url='https://github.com/datamill-co/singer-runner',
+    description='Runs Singer.io taps and targets, adding additional tooling.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=['Programming Language :: Python :: 3 :: Only'],
+    author='datamill',
     version='0.0.1',
     packages=find_packages(),
     install_requires=[
